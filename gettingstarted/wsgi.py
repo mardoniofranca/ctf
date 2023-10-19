4871ef99db447f71f8a086abc0ab98ec28f17a63
mardonio franca <mardfranca@gmail.com>
2023-10-19 10:20:04 -0300

    start

--- gettingstarted/wsgi.py
+++ gettingstarted/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ctf project.
+WSGI config for gettingstarted project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ctf.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
 
 application = get_wsgi_application()
